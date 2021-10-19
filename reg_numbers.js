6289c70ee74e076c7746c9e43d436d4ee39d1b95
amandam2017 <amandamaarman19911@gmail.com>
2021-10-19 12:56:40 +0200

    added yml file for travis

--- reg_numbers.js
+++ reg_numbers.js
@@ -1,104 +1,100 @@
-'use strict';
+"use strict";
 
 module.exports = function reg(pool) {
+  const regqueries = async (toUppReg) => {
+    let regNum = toUppReg.toUpperCase();
 
-	const regqueries = async (toUppReg) => {
-		let regNum = toUppReg.toUpperCase();
-
-		// let pattern1 = /^((CA|CK|CL)\s([0-9]){6})$/;
-		// let pattern2 = /^((CA|CK|CL)\s\d{3}\s\d{3})$/;
-		// let pattern3 = /^((CA|CK|CL)\s\d{3}\-\d{3})$/;
-        
-        // if(regNum && pattern1.test(regNum) || pattern2.test(regNum) || pattern3.test(regNum)){
-            let checkReg = await pool.query('SELECT entered_regs FROM registrations WHERE entered_regs = $1', [regNum]);
-			//adding substringt=to look for the first 2 letter from a town
-			let substring = regNum.substring(0, 2)
-			console.log(substring);
-			var idees = await getId(substring);
-            if(checkReg.rowCount === 0) { //checking if entered reg does not exist and the add it
-                const INSERT_QUERY = await pool.query('INSERT INTO registrations (entered_regs, towns_id) VALUES ($1,$2)', [regNum, idees]);
-            }
-        // }
-	}
-
-	const getId = async (reg_id) => {
-			try {
-			var selectID = await pool.query('SELECT id FROM towns WHERE string_starts_with = $1', [reg_id]);
-			return selectID.rows[0].id;
-
-			
-		} catch (error) {
-			console.log(error)
-			
-		}
-	}
-
-	const setReg = async (plateNumber) => {
-        plateNumber = plateNumber.toUpperCase();
-		await regqueries(plateNumber)
- 
-	}
-
-	const getReg = async () => {
-		try {
-			let regNumberList = await pool.query('SELECT entered_regs FROM registrations')
-			return regNumberList.rows;
-			
-		} catch (error) {
-			console.log(error)
-			
-		}
-		return regNumberList;
-	  }
-
-	  //add list of regs
-	  const list = async (newReg) => {
-		try {
-			let regList = await pool.query('SELECT entered_regs FROM registrations WHERE entered_regs = $1', [newReg])
-			return regList.rows;
-			
-		} catch (error) {
-			console.log(error)
-			
-		}
-		return regList;
-	  }
-
-
-	const filter = async (towns) => {
-		let townStartsWith = await getId(towns)
-		console.log('starts string:' + townStartsWith)
-		let getReg = await pool.query('SELECT entered_regs FROM registrations WHERE towns_id = $1', [townStartsWith])
-		return getReg.rows;
-	}
-
-	const showAll = async () => {
-		try {
-			let allTowns = await pool.query('SELECT entered_regs FROM registrations')
-			return allTowns.rows
-			
-		} catch (error) {			
-		}
-
-	}
-
-	const resert = async () => {
-		try {
-            let clearData = await pool.query('DELETE FROM registrations');
-            return clearData.row;
-        } catch (error) {
-            console.log(error)
-        }
-	}
-
-    return{
-        regqueries,
-        setReg,
-        getReg,
-		getId,
-		filter,
-		showAll,
-		resert,
-		list
+    let checkReg = await pool.query(
+      "SELECT entered_regs FROM registrations WHERE entered_regs = $1",
+      [regNum]
+    );
+    let substring = regNum.substring(0, 2);
+    console.log(substring);
+    var idees = await getId(substring);
+    if (checkReg.rowCount === 0) {
+      const INSERT_QUERY = await pool.query(
+        "INSERT INTO registrations (entered_regs, towns_id) VALUES ($1,$2)",
+        [regNum, idees]
+      );
     }
-}
+  };
+
+  const getId = async (reg_id) => {
+    try {
+      var selectID = await pool.query(
+        "SELECT id FROM towns WHERE string_starts_with = $1",
+        [reg_id]
+      );
+      return selectID.rows[0].id;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const setReg = async (plateNumber) => {
+    plateNumber = plateNumber.toUpperCase();
+    await regqueries(plateNumber);
+  };
+
+  const getReg = async () => {
+    try {
+      let regNumberList = await pool.query(
+        "SELECT entered_regs FROM registrations"
+      );
+      return regNumberList.rows;
+    } catch (error) {
+      console.log(error);
+    }
+    return regNumberList;
+  };
+
+  const list = async (newReg) => {
+    try {
+      let regList = await pool.query(
+        "SELECT entered_regs FROM registrations WHERE entered_regs = $1",
+        [newReg]
+      );
+      return regList.rows;
+    } catch (error) {
+      console.log(error);
+    }
+    return regList;
+  };
+
+  const filter = async (towns) => {
+    let townStartsWith = await getId(towns);
+    console.log("starts string:" + townStartsWith);
+    let getReg = await pool.query(
+      "SELECT entered_regs FROM registrations WHERE towns_id = $1",
+      [townStartsWith]
+    );
+    return getReg.rows;
+  };
+
+  const showAll = async () => {
+    try {
+      let allTowns = await pool.query("SELECT entered_regs FROM registrations");
+      return allTowns.rows;
+    } catch (error) {}
+  };
+
+  const resert = async () => {
+    try {
+      let clearData = await pool.query("DELETE FROM registrations");
+      return clearData.row;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return {
+    regqueries,
+    setReg,
+    getReg,
+    getId,
+    filter,
+    showAll,
+    resert,
+    list,
+  };
+};
--- regnumbers_routes.js
+++ regnumbers_routes.js
@@ -1,129 +1,112 @@
-'use strict';
+"use strict";
 
-module.exports = function Routes(RegFact){
-
+module.exports = function Routes(RegFact) {
   let pattern1 = /^((CA|CK|CL)\s([0-9]){6})$/;
   let pattern2 = /^((CA|CK|CL)\s\d{3}\s\d{3})$/;
   let pattern3 = /^((CA|CK|CL)\s\d{3}\-\d{3})$/;
 
-  let regList = [];
-
-
-    const home = async (req, res) => {
-      try {            
-        res.render("index", {
-          eachRegNo: await RegFact.getReg(),
-        });
-    
-      } catch (error) {
-        console.log(error);
-      }
+  const home = async (req, res) => {
+    try {
+      res.render("index", {
+        eachRegNo: await RegFact.getReg(),
+      });
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const addReg = async (req, res) => {
-      try {
-        let emptyFieldError = '*Please enter a registration number*'
-        let alreadyExistRegError = '*Registration number already exist*'
-        let incorrectRegFormat = '*Please enter reg from these towns in this format [CL 123452] OR [CK 123-321] OR [CL 012 658]*'
-        var reg = req.body.enteredReg
+  const addReg = async (req, res) => {
+    try {
+      let emptyFieldError = "*Please enter a registration number*";
+      let alreadyExistRegError = "*Registration number already exist*";
+      let incorrectRegFormat =
+        "*Please enter reg from these towns in this format [CL 123452] OR [CK 123-321] OR [CL 012 658]*";
+      var reg = req.body.enteredReg;
 
-        let myList = await RegFact.list(reg);
+      let myList = await RegFact.list(reg);
 
-        if(reg){
-          if(reg && pattern1.test(reg) || pattern2.test(reg) || pattern3.test(reg)){
-          if(myList == 0) {
-          
+      if (reg) {
+        if (
+          (reg && pattern1.test(reg)) ||
+          pattern2.test(reg) ||
+          pattern3.test(reg)
+        ) {
+          if (myList == 0) {
             console.log(reg);
-            req.flash('successfulMessage', '*Successfully added registration*');
+            req.flash("successfulMessage", "*Successfully added registration*");
 
             await RegFact.setReg(reg);
 
-            console.log('done adding');
-
-          }else{
-            req.flash('error', alreadyExistRegError)
+            console.log("done adding");
+          } else {
+            req.flash("error", alreadyExistRegError);
           }
-          }else{
-            req.flash('error', incorrectRegFormat);
-          }
-            
-        }else{
-          req.flash('error', emptyFieldError);
-
+        } else {
+          req.flash("error", incorrectRegFormat);
         }
-        // console.log('before redirect');
-        res.redirect('/');
-        
+      } else {
+        req.flash("error", emptyFieldError);
+      }
+
+      res.redirect("/");
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-    }
+  };
 
-    const townRegistrations = async (req, res) => {
-      try {
-        var checkedTown = req.body.town
+  const townRegistrations = async (req, res) => {
+    try {
+      var checkedTown = req.body.town;
 
-        let townReg = await RegFact.filter(checkedTown);
-        if(checkedTown){
-          res.render('index', {
-          townReg
+      let townReg = await RegFact.filter(checkedTown);
+      if (checkedTown) {
+        res.render("index", {
+          townReg,
         });
-      }else{
-          req.flash('error', '*Please select a town*'); 
-          
-          
-        } 
-        
-      } catch (error) {
-        console.log(error)
-        
+      } else {
+        req.flash("error", "*Please select a town*");
       }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    async function addRegsOnUrl(req, res) {
-      try {
-        let regs = req.params.entered_regs;
-        res.render('enteredRegs', {
-          regs,
-        })
-    
-      } catch (error) {
-        console.log(error);
-      }
-    
+  async function addRegsOnUrl(req, res) {
+    try {
+      let regs = req.params.entered_regs;
+      res.render("enteredRegs", {
+        regs,
+      });
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    const showAllRegs = async (req, res) => {
-      try {
-        res.render('index', {
-          eachRegNo: await RegFact.showAll()
+  const showAllRegs = async (req, res) => {
+    try {
+      res.render("index", {
+        eachRegNo: await RegFact.showAll(),
+      });
+    } catch (error) {}
+  };
 
-        })
-        
-      } catch (error) {
-        
-      }
+  const clearDataBase = async (req, res) => {
+    try {
+      await RegFact.resert();
+      req.flash("successfulMessage", "Successfully cleared the database");
+
+      res.redirect("/");
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const clearDataBase = async (req, res) => {
-      try {
-        
-        await RegFact.resert()
-        req.flash('successfulMessage', 'Successfully cleared the database');
-
-    
-        res.redirect('/')
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    return{
-        home,
-        addReg,
-        clearDataBase,
-        townRegistrations,
-        showAllRegs,
-        addRegsOnUrl,
-    }
-}
+  return {
+    home,
+    addReg,
+    clearDataBase,
+    townRegistrations,
+    showAllRegs,
+    addRegsOnUrl,
+  };
+};
