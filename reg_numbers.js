b51c1a88fb06eabdf454b7c3ea93817f796aa439
amandam2017 <amandamaarman19911@gmail.com>
2021-10-19 15:31:52 +0200

    added eslint

--- reg_numbers.js
+++ reg_numbers.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
 module.exports = function reg(pool) {
   const regqueries = async (toUppReg) => {
     let regNum = toUppReg.toUpperCase();
 
     let checkReg = await pool.query(
-      "SELECT entered_regs FROM registrations WHERE entered_regs = $1",
+      'SELECT entered_regs FROM registrations WHERE entered_regs = $1',
       [regNum]
     );
     let substring = regNum.substring(0, 2);
@@ -13,7 +13,7 @@ module.exports = function reg(pool) {
     var idees = await getId(substring);
     if (checkReg.rowCount === 0) {
       const INSERT_QUERY = await pool.query(
-        "INSERT INTO registrations (entered_regs, towns_id) VALUES ($1,$2)",
+        'INSERT INTO registrations (entered_regs, towns_id) VALUES ($1,$2)',
         [regNum, idees]
       );
     }
@@ -22,7 +22,7 @@ module.exports = function reg(pool) {
   const getId = async (reg_id) => {
     try {
       var selectID = await pool.query(
-        "SELECT id FROM towns WHERE string_starts_with = $1",
+        'SELECT id FROM towns WHERE string_starts_with = $1',
         [reg_id]
       );
       return selectID.rows[0].id;
@@ -39,7 +39,7 @@ module.exports = function reg(pool) {
   const getReg = async () => {
     try {
       let regNumberList = await pool.query(
-        "SELECT entered_regs FROM registrations"
+        'SELECT entered_regs FROM registrations'
       );
       return regNumberList.rows;
     } catch (error) {
@@ -51,7 +51,7 @@ module.exports = function reg(pool) {
   const list = async (newReg) => {
     try {
       let regList = await pool.query(
-        "SELECT entered_regs FROM registrations WHERE entered_regs = $1",
+        'SELECT entered_regs FROM registrations WHERE entered_regs = $1',
         [newReg]
       );
       return regList.rows;
@@ -63,9 +63,9 @@ module.exports = function reg(pool) {
 
   const filter = async (towns) => {
     let townStartsWith = await getId(towns);
-    console.log("starts string:" + townStartsWith);
+    console.log('starts string:' + townStartsWith);
     let getReg = await pool.query(
-      "SELECT entered_regs FROM registrations WHERE towns_id = $1",
+      'SELECT entered_regs FROM registrations WHERE towns_id = $1',
       [townStartsWith]
     );
     return getReg.rows;
@@ -73,14 +73,14 @@ module.exports = function reg(pool) {
 
   const showAll = async () => {
     try {
-      let allTowns = await pool.query("SELECT entered_regs FROM registrations");
+      let allTowns = await pool.query('SELECT entered_regs FROM registrations');
       return allTowns.rows;
     } catch (error) {}
   };
 
   const resert = async () => {
     try {
-      let clearData = await pool.query("DELETE FROM registrations");
+      let clearData = await pool.query('DELETE FROM registrations');
       return clearData.row;
     } catch (error) {
       console.log(error);
